f1d27af5d44f038fb6e08cfc82d410b40102faed
Tamagotchi9 <voloshko999@gmail.com>
2023-09-13 23:04:36 +0300

    add cryptonomicon

---
+++ src/components/AddTicker.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AddTicker"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TickerGraph.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TickerGraph"
+}
+</script>
+
+<style scoped>
+
+</style>
